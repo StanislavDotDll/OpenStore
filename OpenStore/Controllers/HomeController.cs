653c88d15e30e5d0599cf6860f5004511032bbc2
StanislavDotDll <63843646+StanislavDotDll@users.noreply.github.com>
2020-12-13 00:17:16 +0200

    Add files via upload

---
+++ OpenStore/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenStore.Model;
+
+namespace OpenStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductContext db;
+        public HomeController(ProductContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Сabinet()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult ViewProduct(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.ProductId = id;
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.ProductId = id;           
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+
+
+        
+
+    }
+}
