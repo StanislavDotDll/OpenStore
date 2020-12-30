bf9e90b95243e490573e8ce138910624e759cf3f
StanislavDotDll <63843646+StanislavDotDll@users.noreply.github.com>
2020-12-30 07:59:25 +0200

    Add files via upload

---
+++ OpenStore v1.1Dark/OpenStore/Controllers/HomeController.cs
@@ -0,0 +1,110 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OpenStore.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace OpenStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        ProductContext db;
+        public HomeController(ProductContext context)
+        {
+            db = context;
+        }
+        public IActionResult Index()
+        {
+            return View(db.Products.ToList());
+        }
+
+        public IActionResult Authorization()
+        {
+            return View();
+        }
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        public IActionResult Registration()
+        {
+            return View();
+        }
+
+        public IActionResult Сabinet()
+        {
+            return View(db.Products.ToList());
+        }
+
+
+        [HttpGet]
+        public IActionResult ViewProduct(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.ProductId = id;
+            return View(db.Products.ToList());
+        }
+
+
+        [HttpGet]
+        public IActionResult Buy(int? id)
+        {
+            if (id == null) return RedirectToAction("Index");
+            ViewBag.ProductId = id;           
+            return View();
+        }
+        [HttpPost]
+        public string Buy(Order order)
+        {
+            db.Orders.Add(order);
+            db.SaveChanges();
+            return "Спасибо, " + order.User + ", за покупку!";
+        }
+
+     
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id != null)
+            {
+                Product product = await db.Products.FirstOrDefaultAsync(p => p.Id == id);
+                if (product != null)
+                {
+                    foreach(var item in db.Products)
+                    {
+                        if(id==item.Id)
+                        {
+                            ViewBag.ProductId = id;
+                            ViewBag.ProductName =item.Name;
+                            ViewBag.ProductImageSrc = item.ImageSrc;
+                            ViewBag.ProductPrice = item.Price;
+                            ViewBag.ProductQuantity = item.Quantity;
+                            ViewBag.ProductShortDescribe = item.ShortDescribe;
+                            ViewBag.ProductDescribe = item.Describe;
+                            ViewBag.ProductBrand = item.Brand;
+                            ViewBag.ProductCharacteristics = item.Characteristics;
+
+                        }
+                    }
+             
+                    return View(product);
+                }
+                
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Edit(Product product)
+        {
+            db.Products.Update(product);
+            await db.SaveChangesAsync();
+            return RedirectToAction("Index");
+        }
+
+
+
+    }
+}
---
+++ OpenStore v1.1Dark/OpenStore/Model/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OpenStore.Model
+{
+    public class Product
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageSrc { get; set; }
+        public int Price { get; set; }
+        public int Quantity { get; set; }
+        public string ShortDescribe { get; set; }
+        public string Describe { get; set; }
+        public string Brand { get; set; }
+        public string Characteristics { get; set; }
+
+    }
+}
